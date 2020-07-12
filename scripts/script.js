2410a51dd17c48afd60d4d6671d5ca492cd1fb7f
rodrigoabam <rodrigoab.am@gmail.com>
2020-07-11 21:31:51 -0400

    pag pesquisa e resultado de pesquisa

--- scripts/script.js
+++ scripts/script.js
@@ -4,6 +4,8 @@ for(const card of cards){
     card.addEventListener("click", cardSelected)
 }
 
+const collectedCards = document.querySelector("input[name=cards]")
+
 let selectedCards = []
 
 function cardSelected(event) {
@@ -11,5 +13,24 @@ function cardSelected(event) {
     const cardId = cardLi.dataset.id
 
     cardLi.classList.toggle("selected")
+
+    const alreadySelected = selectedCards.findIndex( card => {
+        const cardFound = card === cardId 
+        return cardFound
+    } )
+
+    if(alreadySelected >= 0){
+        const filteredCards = selectedCards.filter( card => {
+            const cardIsDifferent = card != cardId
+            return cardIsDifferent
+        } )
+
+        selectedCards = filteredCards
+    } else {
+        selectedCards.push(cardId)
+    }
+
+    collectedCards.value = selectedCards
+
 } 
 
