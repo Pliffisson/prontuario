77c7b244990e9374c62ac856fea43c2adfb37c34
rodrigoabam <rodrigoab.am@gmail.com>
2020-07-12 21:07:54 -0400

    pag paciente e busca

---
+++ scripts/search-result.js
@@ -0,0 +1,9 @@
+const resPatients = document.querySelectorAll("main .info")
+
+for(let resPatient of resPatients) {
+    resPatient.addEventListener("click", () => {
+      window.location.href = "patient.html"
+    })
+}
+
+
